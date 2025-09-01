1fa4110a6ec92c33c4d03631ab8bd04b9beacc97
neomn110 <neomn110@gmail.com>
2025-09-01 13:01:18 +0330

    refactor: remove all ansible files and docker related stuffs

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM neomn110/ansible:0.0.1
-
-RUN apk update && apk add \
-    git
-    
-COPY ./ansible /ansible 
-COPY ./files /files 
-
-CMD ["/files/scripts/docker-cmd-script.sh"]
--- files/scripts/container-startup-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-eval $(ssh-agent)
-ssh-add /files/.secrets/ansible-course-ssh-key
-export ANSIBLE_CONFIG=/ansible/ansible.cfg
-export $(grep -v '^#' /files/.secrets/credentials.txt |xargs)
-fish -c "echo run a fish command to create fish config directory"
-cat /files/scripts/alias | tee > /root/.config/fish/config.fish
-fish
